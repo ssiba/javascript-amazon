4a09668cc0e8741c5473b2983ae6b34fbd4dbb3b
Siba Sankar Gouda <sibasankargouda@Sibas-MacBook-Air.local>
2025-12-23 13:45:27 +0530

    Save cart into localstorage

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,7 @@
-export let cart = [
+export let cart = 
+JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart = [
     {
         productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
         quantity:2  
@@ -8,7 +11,11 @@ export let cart = [
         quantity:1
     }
 ];
+}
 
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 export function addToCart(productId){
     let matchingItem;
     
@@ -26,6 +33,7 @@ export function addToCart(productId){
         quantity: 1
       });
     }
+    saveToStorage();
   }
   export function removeFromCart(productId){
     const newCart = [];
@@ -36,4 +44,5 @@ export function addToCart(productId){
     });
 
     cart = newCart;
+    saveToStorage();
   }
